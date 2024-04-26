2662813c5d20b4ccfd91063ea6558a0cef866e2f
Durkich <romeo-092002@inbox.ru>
2024-04-26 15:34:04 +0300

    repositories final update

--- Computers-service/src/main/java/com/Kalabekov/Computersservice/model/Peripheral.java
+++ Computers-service/src/main/java/com/Kalabekov/Computersservice/model/Peripheral.java
@@ -1,12 +1,21 @@
 package com.Kalabekov.Computersservice.model;
+import jakarta.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter @ToString @Getter
+@Entity
+@Table(name = "peripherals")
 public class Peripheral {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "peripheral_name")
     private String peripheralName;
+    @Column(name = "peripheral_type")
     private String PeripheralType;
+    @Column(name = "computer_id")
     private int computerId;
 }
--- Computers-service/src/main/java/com/Kalabekov/Computersservice/model/Software.java
+++ Computers-service/src/main/java/com/Kalabekov/Computersservice/model/Software.java
@@ -1,13 +1,23 @@
 package com.Kalabekov.Computersservice.model;
+import jakarta.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter @ToString @Getter
+@Entity
+@Table(name = "software")
 public class Software {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "software_name")
     private String softwareName;
+    @Column(name = "software_version")
     private String softwareVersion;
+    @Column(name = "is_licensed")
+    private boolean isLicensed;
+    @Column(name = "computer_id")
     private int computerId;
 }
