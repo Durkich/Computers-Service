1c6b8dbb1d8b02bb10c6068eebfc4165f778b799
Durkich <romeo-092002@inbox.ru>
2024-05-14 17:16:36 +0300

    config

---
+++ configserver/src/test/java/com/Computersservice/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Computersservice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
