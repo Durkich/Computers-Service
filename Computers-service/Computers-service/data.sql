0e4ea749378d4657cd9aec72c7358e4600670330
Durkich <romeo-092002@inbox.ru>
2024-05-15 14:50:46 +0300

    work in progress

---
+++ Computers-service/Computers-service/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO infrasctructure.computers VALUES (1,'128g','GTX950','i5-4460',128,8);
+INSERT INTO infrasctructure.computers VALUES (2,'Блабла,NVIDIA GeForce RTX 3080','Intel Core i9-10900K');
---
+++ Computers-service/Computers-service/init.sql
@@ -0,0 +1,82 @@
+CREATE SCHEMA infrastructure;
+CREATE TABLE IF NOT EXISTS infrastructure.computers
+(
+    id integer COLLATE pg_catalog."default",
+    inventory_number character varying(255) COLLATE pg_catalog."default",
+    graphic_adapter character varying(255) COLLATE pg_catalog."default",
+    processor character varying(255) COLLATE pg_catalog."default",
+    rom integer,
+    ram integer,
+    CONSTRAINT computers_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS infrastructure.computers
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS infrastructure.laptops
+(
+    id integer COLLATE pg_catalog."default",
+    inventory_number character varying(255) COLLATE pg_catalog."default",
+    graphic_adapter character varying(255) COLLATE pg_catalog."default",
+    processor character varying(255) COLLATE pg_catalog."default",
+    rom integer,
+    ram integer,
+    display character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT laptops_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS infrastructure.laptops
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS infrastructure.peripherals
+(
+    id integer COLLATE pg_catalog."default",
+    peripheral_name character varying(255) COLLATE pg_catalog."default",
+    peripheral_type character varying(255) COLLATE pg_catalog."default",
+    computer_id integer,
+    laptop_id integer,
+    CONSTRAINT peripherals_pkey PRIMARY KEY (id),
+    CONSTRAINT computer_id FOREIGN KEY (computer_id)
+        REFERENCES infrastructure.computers (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT laptop_id FOREIGN KEY (laptop_id)
+        REFERENCES infrastructure.laptops (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS infrastructure.peripherals
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS infrastructure.software
+(
+    id integer COLLATE pg_catalog."default",
+    software_name character varying(255) COLLATE pg_catalog."default",
+    software_version character varying(255) COLLATE pg_catalog."default",
+    is_licensed boolean,
+    computer_id integer,
+    laptop_id integer,
+    CONSTRAINT software_pkey PRIMARY KEY (id),
+    CONSTRAINT computer_id FOREIGN KEY (computer_id)
+        REFERENCES infrastructure.computers (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT laptop_id FOREIGN KEY (laptop_id)
+        REFERENCES infrastructure.laptops (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS infrastructure.software
+    OWNER to postgres;
